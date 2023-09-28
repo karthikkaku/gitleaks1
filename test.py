f0939448f6c16c544c5041683cb1e794194839ac
karthikkaku <128590795+karthikkaku@users.noreply.github.com>
2023-09-28 15:09:20 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-API_KEY = "my_api_key"
-PASSWORD = "my_password"
+API_KEY = "cef564e4-0b96-43ea-9283-5456c35827bf"
+
+# Dummy Database Password
+DB_PASSWORD = "eb8b4ba2-7809-4512-8b6b-9c23cc02f8fc"
+
+# Dummy JWT Secret Key
+JWT_SECRET = "50eeb54c-cc92-401c-88dc-87ca29601c8e"
+
+# Dummy Encryption Key
+ENCRYPTION_KEY = "2a35044f-9873-4edc-98a7-9fe84397f608"
+
+# Dummy SMTP Password
+SMTP_PASSWORD = "6a9340bc-ac53-4ecf-814a-15f74a78f4fc"
